7e86f77a0e81e16db84a3353abacd4632a4b7361
Franci Nakamura <francinakamura@FranciNrasiMac3.attlocal.net>
2020-03-12 12:08:15 -0700

    Added today, tomorrow, next week

---
+++ src/TodoItem.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+function TodoItem (){
+    
+    return (
+
+    <div className ="todo-item"> 
+      <input type ="checkbox"/>
+      <p>Today</p> 
+
+      <input type ="checkbox"/>
+      <p>Tomorrow</p> 
+      
+      <input type ="checkbox"/>
+      <p>Next Week</p> 
+
+    </div>
+    
+    )
+    }
+    
+
+
+export default TodoItem;
